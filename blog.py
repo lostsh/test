328ad4721d9b271b3a2d5fc7454c16d84261e427
lostsh <0pdlna3@gmail.com>
2020-10-17 21:37:15 +0200

    update python prog

---
+++ blog.py
@@ -0,0 +1,14 @@
+file = open("index.html", "w+")
+
+file.write("<!DOCTYPE html>\n\
+<html>\n\
+    <head>\n\
+        <meta charset=\"utf-8\"/>\n\
+        <title>Py Builded</title>\n\
+    </head>\n\
+    <body>\n\
+        <h1>Hello word</h1>\n\
+    </body>\n\
+</html>")
+
+file.close()
