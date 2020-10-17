003491c4c287baa9bc110997549a1abf4672cd78
lostsh <0pdlna3@gmail.com>
2020-10-18 00:16:08 +0200

    pytest

--- blog.py
+++ blog.py
@@ -1,3 +1,9 @@
+import os
+
+def aTag(link, text):
+    return "<a href="+link+">"+text+"</a>"
+#def navTag()
+
 def build(fName):
     indexTemplate = open(fName[:fName.index(".")]+"-template"+fName[fName.index("."):], "r")
     index = open(fName, "w")
@@ -7,9 +13,15 @@ def build(fName):
             l = l.replace("[TITLE]", "This is the title of ths blog")
         elif "[PARAGRAPH]" in l:
             l = l.replace("[PARAGRAPH]", "Hello world I am lama !")
+        elif "[NAV]" in l:
+            strNav = ""
+            for i in os.listdir("./"):
+                if ".html" in i and not "-template" in i: strNav += aTag(i, i[:i.index(".")])
+            l = l.replace("[NAV]", strNav)
         index.write(l)
     
     index.close()
     indexTemplate.close()
+
 
 build("index.html")
